75c11f11b025d48ab916046085415e5c3e9fd254
tiway <stilon670@gmail.com>
2024-05-19 13:19:09 +0300

    create class TaskListView

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from web.views import TaskListView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("todo-list/", TaskListView.as_view(), name="task-list"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from web.models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "web/task_list.html"
+    context_object_name = "task_list"
+    paginate_by = 5
+
+    class Meta:
+        ordering = ["-deadline"]
