06450e2b6aa27d613524c73f82f06b09a2b5023c
tiway <stilon670@gmail.com>
2024-05-19 15:59:35 +0300

    create urls.py to web

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,19 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
-from web.views import (
-    TaskListView,
-    TaskCreateView,
-    TagsListView, TaskUpdateView, TaskDeleteView,
-)
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("todo-list/", TaskListView.as_view(), name="task-list"),
-    path("task/create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
-    path("tags/", TagsListView.as_view(), name="tags-list"),
+    path("", include("web.urls", namespace="web"))
 ]
