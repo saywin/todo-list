c8d68a6b0fc62bac7ebdc4392ac4a4f8b9f73ae4
tiway <stilon670@gmail.com>
2024-05-19 12:58:17 +0300

    add classes TaskAdmin and TagAdmin to admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        "content", "datetime", "is_done", "deadline",
+    )
+    search_fields = ("content", )
+    list_filter = ("is_done", "deadline",)
