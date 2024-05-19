96e39636fb076e952bbc01f371d1712bf25ff2dd
tiway <stilon670@gmail.com>
2024-05-19 15:47:52 +0300

    install and settings crispy

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "web",
+    "crispy_forms",
+    "crispy_bootstrap4",
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,6 @@ STATICFILES_DIRS = [BASE_DIR / "static", ]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
