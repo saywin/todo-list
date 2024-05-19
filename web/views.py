6018ebcdc0862ec503d91ba403ad99811b5bf089
tiway <stilon670@gmail.com>
2024-05-19 16:29:17 +0300

    add classes TagCreateView, TagUpdateView and TagDeleteView to views.py

--- web/views.py
+++ web/views.py
@@ -17,15 +17,6 @@ class TaskListView(generic.ListView):
         ordering = ["-deadline"]
 
 
-class TagsListView(generic.ListView):
-    model = Tag
-    template_name = "web/tags_list.html"
-    context_object_name = "tags_list"
-
-    class Meta:
-        ordering = ["name"]
-
-
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
@@ -50,3 +41,29 @@ class StatusTask(View):
         task.is_done = not task.is_done
         task.save()
         return redirect("web:task-list")
+
+
+class TagsListView(generic.ListView):
+    model = Tag
+    template_name = "web/tags_list.html"
+    context_object_name = "tags_list"
+
+    class Meta:
+        ordering = ["name"]
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("web:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("web:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("web:tag-list")
