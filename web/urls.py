2bf0bd3df167936e4bbdbadf48d7dbd4785f4a21
tiway <stilon670@gmail.com>
2024-05-19 16:25:49 +0300

    implemented so that the status changes to views.py

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from web.views import (
     TaskListView,
     TaskCreateView,
-    TagsListView, TaskUpdateView, TaskDeleteView,
+    TagsListView,
+    TaskUpdateView,
+    TaskDeleteView,
+    StatusTask,
 )
 
 urlpatterns = [
@@ -11,6 +14,7 @@ urlpatterns = [
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
+    path("status/<int:pk>/", StatusTask.as_view(), name="task-status"),
     path("tags/", TagsListView.as_view(), name="tags-list"),
 ]
 
--- web/views.py
+++ web/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+
+from django.shortcuts import redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views import generic
+from django.utils.decorators import method_decorator
+from django.views import generic, View
+from django.views.decorators.csrf import csrf_exempt
 
 from web.models import Task, Tag
 
@@ -26,16 +29,24 @@ class TagsListView(generic.ListView):
 class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
-    success_url = reverse_lazy("web:task_list")
+    success_url = reverse_lazy("web:task-list")
+
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
     fields = "__all__"
-    success_url = reverse_lazy("web:task_list")
+    success_url = reverse_lazy("web:task-list")
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
-    success_url = reverse_lazy("web:task_list")
+    success_url = reverse_lazy("web:task-list")
 
 
+# @method_decorator(csrf_exempt, name="post")
+class StatusTask(View):
+    def get(self, request, pk):
+        task = get_object_or_404(Task, id=pk)
+        task.is_done = not task.is_done
+        task.save()
+        return redirect("web:task-list")
