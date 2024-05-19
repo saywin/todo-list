abecf2181fc9ea47de68a739c1b6ad7b184e2b93
tiway <stilon670@gmail.com>
2024-05-19 16:52:26 +0300

    small edits in styles

--- web/urls.py
+++ web/urls.py
@@ -13,15 +13,15 @@ from web.views import (
 )
 
 urlpatterns = [
-    path("todo-list/", TaskListView.as_view(), name="task-list"),
+    path("", TaskListView.as_view(), name="task-list"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/<int:pk>/update", TaskUpdateView.as_view(), name="task-update"),
     path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
     path("status/<int:pk>/", StatusTask.as_view(), name="task-status"),
     path("tags/", TagsListView.as_view(), name="tags-list"),
-    path("tag/create/", TagCreateView.as_view(), name="tag-create"),
-    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
 
 app_name = "web"
